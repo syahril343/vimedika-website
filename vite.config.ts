6ff01f4f6436ad0acbafbba25b8cd318378163f0
unknown <syahril.21058@mhs.unesa.ac.id>
2025-07-09 16:24:59 +0700

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // penting agar bisa diakses dari luar kontainer
+    port: 5173,
+    allowedHosts: ['vimedika.com'], // <--- tambahkan ini
+  },
 })
